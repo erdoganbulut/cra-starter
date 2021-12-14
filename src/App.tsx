819f323aa0740f104b1c4099185c0e0069cce354
Erdoğan Bulut <e-bulut@hotmail.com.tr>
2021-12-14 21:55:26 +0300

    Add eslint support

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      App works!
-    </div>
-  );
-}
+const App = function () {
+  return <div className="App">App works!</div>;
+};
 
 export default App;
