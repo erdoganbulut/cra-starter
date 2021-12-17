761f07803f822e28729227b8be84e6239d0427f3
Erdoğan Bulut <e-bulut@hotmail.com.tr>
2021-12-17 20:11:23 +0300

    Install ant design

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
+import './styles/vendors.scss';
 import './styles/global.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
